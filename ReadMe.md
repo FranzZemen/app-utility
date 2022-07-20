11d5af28a7fbac5ce78c1fafa483f0896854992f
Franz Zemen <franzzemen@gmail.com>
2022-07-20 13:06:01 -0400

    Moving to public

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Read Me
+This package supports @franzzemen projects.  It is not intended to be utilized by itself, though some functionality 
+here may be of use.
--- ts-src/execution-context.ts
+++ ts-src/execution-context.ts
@@ -5,7 +5,7 @@ import {AppConfigI, appConfigSchema} from './app-config'
 export interface ExecutionContextI {
   appContext?: string; // The application context, for example, butchersrow
   thread?: string; // The thread of execution, which can span across processes (if supplied in headers).
-  requestId?: string; // The current process request id, as formed fromcd  the aws request id.
+  requestId?: string; // The current process request id, as formed from  the aws request id.
   authorization?: string; // Optional authorization in Bearer Token format Bearer [jwt]
   localContext?: string; // Optional local context - user beware of tracking this!
   usePromises?: boolean; // Optional, where promises are an option, use them if true
