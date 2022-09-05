3bf2ad61ca489adb1d5e29afe8d2b96096e34c37
Franz Zemen <franzzemen@gmail.com>
2022-09-05 16:24:06 -0400

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,25 +1,31 @@
-import {cwd} from 'process';
+import {cwd} from 'node:process';
 import * as gulpBase from '@franzzemen/gulp-base';
 import { createRequire } from "module";
-import {join} from 'path';
-import * as npmuFunc from '@franzzemen/npmu';
+import {join, dirname} from 'node:path';
+import {npmu as npmuFunc} from '@franzzemen/npmu';
+import {fileURLToPath} from 'url';
+
 const requireModule = createRequire(import.meta.url);
 gulpBase.init(requireModule('./package.json'), cwd() + '/tsconfig.src.json', cwd() + '/tsconfig.test.json', 100);
 gulpBase.setMainBranch('main');
 
 
-export const npmu  = (cb) => npmuFunc([
-  {
-    path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/gulp-base',
-  }, {
-    path: join(__dirname, '../npmu'), packageName: '@franzzemen/npmu',
-  }, {
-    path: join(__dirname, './'), packageName: '@franzzemen/app-utility',
-  }])
-  .then(() => {
-console.log('cb...');
-cb();
-});
+export const npmu  = (cb) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  npmuFunc([
+    {
+      path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/gulp-base',
+    }, {
+      path: join(__dirname, '../npmu'), packageName: '@franzzemen/npmu',
+    }, {
+      path: join(__dirname, './'), packageName: '@franzzemen/app-utility',
+    }])
+    .then(() => {
+      console.log('cb...');
+      cb();
+    })
+}
 
 
 
