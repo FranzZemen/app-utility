64853df21a9a1106325928c60978325b97b060c4
Franz Zemen <franzzemen@gmail.com>
2022-10-10 09:55:51 -0400

    isAppConfigSync

--- ts-src/app-config.ts
+++ ts-src/app-config.ts
@@ -1,4 +1,5 @@
 import Validator, {ValidationError, ValidationSchema} from 'fastest-validator';
+import {isPromise} from 'node:util/types';
 import {LogConfigI, logConfigSchema} from './log/index.js';
 
 export interface AppConfigI {
@@ -18,5 +19,9 @@ const check = (new Validator()).compile({app: appConfigSchema});
 
 export function validateAppConfig(appConfig: AppConfigI): ValidationError[] | true | Promise<true | ValidationError[]> {
   return check(appConfig);
+}
+
+export function isAppConfigSync(config: any | AppConfigI): config is AppConfigI {
+  return validateAppConfig(config) === true;
 }
 
